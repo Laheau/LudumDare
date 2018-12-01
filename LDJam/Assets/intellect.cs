a994d59a0c1bcf8d9d3dcfb2247a34522e0cf55f
Laheau <Laheau@users.noreply.github.com>
2018-11-30 21:52:26 -0500

    script

---
+++ LDJam/Assets/intellect.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intellect : MonoBehaviour {
+
+    public static int nb;
+    public static float valeur = 2f;
+	
+}
---
+++ LDJam/Assets/worker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worker : MonoBehaviour {
+
+    public static int nb;
+    public static float valeur = 2;
+}
